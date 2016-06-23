78c51872c49ec9977887b87aba26058f3f62e4b9
sukhesh <hsukhesh@yahoo.com>
2016-06-23 21:18:58 +0530

    for loop iterations in c#

---
+++ ForIterations/ForIterations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<10; i++)
+            {
+                Console.WriteLine(i);
+
+                if(i == 7)
+                {
+                    Console.WriteLine("Seven Found...!");
+                    break;
+                }
+            }
+           
+            for (int myValue = 0; myValue < 11; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+            Console.ReadLine();
+        }
+    
+    }
+   
+}
